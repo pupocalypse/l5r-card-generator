5aea3d782afb12bfcdaeb9813541cbe842c07fb2
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2023-10-21 15:54:31 -0700

    it's been a while - committing previous changes before overhaul

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,97 +1,98 @@
-import {
-  useState,
-  useEffect,
-  useRef,
-  useContext,
-} from "react/cjs/react.development";
+import { useState, useEffect, useRef, useContext } from "react";
 import { CardContext } from "../App";
 import { absolutePosition } from "../js/utility";
 
+const sliderDefault = 120;
+
 function ImageUpload() {
   const { cardDetails, setCardDetails } = useContext(CardContext);
+  const [imagePos, setImagePos] = useState(cardDetails.photo.position);
+  const [sliderVal, setSliderVal] = useState(
+    cardDetails.photo.scale || sliderDefault
+  );
+  const [sliderMax, setSliderMax] = useState(400);
   let imageRef = useRef(null),
-    resetRef = useRef(null),
-    resizeRef = useRef(null),
     liveAreaRef = useRef(null),
-    liveAreaRectRef = useRef(null);
+    liveAreaRect = () => absolutePosition(liveAreaRef.current);
   let startX = 0,
     startY = 0,
     newX = 0,
     newY = 0;
-  const [imagePos, setImagePos] = useState(cardDetails.photo.position);
-  const [sliderVal, setSliderVal] = useState(cardDetails.photo.scale);
 
-  // adjust image size on slider change
-  useEffect(() => {
-    liveAreaRectRef.current = absolutePosition(liveAreaRef.current);
-    let sliderMax = liveAreaRectRef.current.width * 4;
-
-    if (imageRef.current.style.width > imageRef.current.style.height) {
-      imageRef.current.classList.add("horizontal");
-    } else if (imageRef.current.style.height > imageRef.current.style.width) {
-      imageRef.current.classList.add("vertical");
-    } else {
-      // default horizontal for square images
-      imageRef.current.classList.add("horizontal");
-    }
-
-    // change slider max value if intrinsic image size is less
-    if (
-      imageRef.current.naturalWidth < sliderMax ||
-      imageRef.current.naturalHeight < sliderMax
-    ) {
-      let newMax;
-      if (
-        imageRef.current.naturalWidth < sliderMax &&
-        imageRef.current.naturalWidth <= imageRef.current.naturalHeight
-      ) {
-        // if width is the smaller value...
-        newMax =
-          Math.floor(
-            (imageRef.current.naturalWidth / liveAreaRectRef.current.width) * 10
-          ) * 10;
-      } else if (imageRef.current.naturalHeight < sliderMax) {
-        // if height is the smaller value...
-        newMax =
-          Math.floor(
-            (imageRef.current.naturalHeight / liveAreaRectRef.current.height) *
-              10
-          ) * 10;
-      }
-
-      resizeRef.current.setAttribute("max", newMax);
-    }
-
-    previewImage();
-  }, [sliderVal]);
-
+  // position image on mount, or
   // store current image size and position in cardDetails before unmounting
   useEffect(() => {
-    console.log("inside mounting useEffect");
-    console.log("current cardDetails.photo:", cardDetails.photo);
-    const imageRect = absolutePosition(imageRef.current);
-    // console.log("imageRect:", imageRect);
+    // console.log("inside mounting effect");
+    // const imageRect = absolutePosition(imageRef.current);
+    // TO DO - set top and left position somewhere in imageRect or imageRef.current
+    // imageRef.current.style.top = imagePos.top + "px";
+    // imageRef.current.style.left = imagePos.left + "px";
+    // if (imageRef.current.classList.contains("horizontal")) {
+    //   imageRef.current.style.height = `${sliderVal}%`;
+    // }
+    // if (imageRef.current.classList.contains("vertical")) {
+    //   imageRef.current.style.width = `${sliderVal}%`;
+    // }
+
     return () => {
-      console.log("cleanup function running");
-      console.log("sliderVal:", sliderVal);
-      // console.log("imageRef.current.style.top:", imageRef.current.style.top);
-      // console.log("imageRef.current.style.left:", imageRef.current.style.left);
+      // console.log("cleanup function running");
       cardDetails.photo.position = {
-        top: Math.floor(imageRect.top),
-        left: Math.floor(imageRect.left),
+        top: Math.floor(imagePos.top),
+        left: Math.floor(imagePos.left),
       };
       cardDetails.photo.scale = sliderVal;
       setCardDetails(cardDetails);
-      console.log("updated cardDetails.photo:", cardDetails.photo);
     };
+  }, []);
+
+  // adjust image size & slider max on slider change
+  useEffect(() => {
+    // console.log("inside image resize useEffect");
+    // declared inside useEffect to counter dependency warnings
+    const changeSliderMax = () => {
+      // console.log("inside changeSliderMax");
+      const liveArea = liveAreaRect();
+      const maxWidthPixels = liveArea.width * 4;
+      let newMax;
+
+      // change slider max value if intrinsic image size is less
+      if (
+        imageRef.current.naturalWidth < maxWidthPixels ||
+        imageRef.current.naturalHeight < maxWidthPixels
+      ) {
+        if (
+          imageRef.current.naturalWidth < maxWidthPixels &&
+          imageRef.current.naturalWidth <= imageRef.current.naturalHeight
+        ) {
+          // if width is the smaller value...
+          newMax =
+            Math.floor((imageRef.current.naturalWidth / liveArea.width) * 10) *
+            10;
+        } else if (imageRef.current.naturalHeight < maxWidthPixels) {
+          // if height is the smaller value...
+          newMax =
+            Math.floor(
+              (imageRef.current.naturalHeight / liveArea.height) * 10
+            ) * 10;
+        }
+
+        setSliderMax(newMax);
+      }
+    };
+
+    changeSliderMax();
+    setImageDirection();
   }, [sliderVal]);
 
-  const previewImage = (e) => {
-    if (imageRef.current.width > imageRef.current.height) {
+  const setImageDirection = (e) => {
+    // console.log("inside setImageDirection");
+    if (imageRef.current.width >= imageRef.current.height) {
+      // default horizontal for square images
       imageRef.current.classList.add("horizontal");
-    }
-    if (imageRef.current.height > imageRef.current.width) {
+      imageRef.current.classList.remove("vertical");
+    } else {
       imageRef.current.classList.add("vertical");
+      imageRef.current.classList.remove("horizontal");
     }
   };
 
@@ -99,12 +100,12 @@ function ImageUpload() {
     imageRef.current.style.top = "50%";
     imageRef.current.style.left = "50%";
     imageRef.current.style.transform = "translate(-50%, -50%)";
-    resizeRef.current.value = 120;
+    setSliderVal(sliderDefault);
     if (imageRef.current.classList.contains("horizontal")) {
-      imageRef.current.style.height = "120%";
+      imageRef.current.style.height = `${sliderDefault}%`;
     }
     if (imageRef.current.classList.contains("vertical")) {
-      imageRef.current.style.width = "120%";
+      imageRef.current.style.width = `${sliderDefault}%`;
     }
   };
 
@@ -116,61 +117,68 @@ function ImageUpload() {
     startY = e.clientY;
 
     const imageRect = absolutePosition(imageRef.current);
+    const liveArea = liveAreaRect();
 
     if (
-      imageRect.left - newX <= Math.ceil(liveAreaRectRef.current.left) &&
-      imageRect.right - newX >= Math.floor(liveAreaRectRef.current.right)
+      imageRect.left - newX <= Math.ceil(liveArea.left) &&
+      imageRect.right - newX >= Math.floor(liveArea.right)
     ) {
       imageRef.current.style.left = imageRef.current.offsetLeft - newX + "px";
     }
     if (
-      imageRect.top - newY <= Math.ceil(liveAreaRectRef.current.top) &&
-      imageRect.bottom - newY >= Math.floor(liveAreaRectRef.current.bottom)
+      imageRect.top - newY <= Math.ceil(liveArea.top) &&
+      imageRect.bottom - newY >= Math.floor(liveArea.bottom)
     ) {
       imageRef.current.style.top = imageRef.current.offsetTop - newY + "px";
     }
+
+    setImagePos({
+      top: parseInt(imageRef.current.style.top, 10),
+      left: parseInt(imageRef.current.style.left, 10),
+    });
   };
 
   const resizeImage = (e) => {
-    resizeRef.current.value = e.target.value;
-    const newVal = resizeRef.current.value + "%";
+    // console.log("inside resizeImage");
+    const newVal = e.target.value;
 
     if (imageRef.current.classList.contains("horizontal")) {
-      imageRef.current.style.height = newVal;
+      imageRef.current.style.height = `${newVal}%`;
       // to do - resize result image
     }
     if (imageRef.current.classList.contains("vertical")) {
-      imageRef.current.style.width = newVal;
+      imageRef.current.style.width = `${newVal}%`;
       // to do - resize result image
     }
 
     const imageRect = absolutePosition(imageRef.current);
+    const liveArea = liveAreaRect();
 
     // check if new edges of image rect are within live area rect, set to closest edge if not
     let newTop, newLeft;
 
     // manage left and right edges out of bounds
-    // find new 'left' value from difference between imageRect and liveAreaRect, add or subtract from actual calculated left as necessary
-    if (imageRect.left > liveAreaRectRef.current.left) {
+    // find new 'left' value from difference between imageRect and liveArea, add or subtract from actual calculated left as necessary
+    if (imageRect.left > liveArea.left) {
       newLeft =
-        parseInt(imageRef.current.style.left) -
-        Math.ceil(imageRect.left - liveAreaRectRef.current.left);
-    } else if (imageRect.right < liveAreaRectRef.current.right) {
+        parseInt(imageRef.current.style.left, 10) -
+        Math.ceil(imageRect.left - liveArea.left);
+    } else if (imageRect.right < liveArea.right) {
       newLeft =
-        parseInt(imageRef.current.style.left) +
-        Math.ceil(liveAreaRectRef.current.right - imageRect.right);
+        parseInt(imageRef.current.style.left, 10) +
+        Math.ceil(liveArea.right - imageRect.right);
     }
 
     // manage top and bottom edges out of bounds
-    // find new 'top' value from difference between imageRect and liveAreaRect, add or subtract from actual calculated top as necessary
-    if (imageRect.top > liveAreaRectRef.current.top) {
+    // find new 'top' value from difference between imageRect and liveArea, add or subtract from actual calculated top as necessary
+    if (imageRect.top > liveArea.top) {
       newTop =
-        parseInt(imageRef.current.style.top) -
-        Math.ceil(imageRect.top - liveAreaRectRef.current.top);
-    } else if (imageRect.bottom < liveAreaRectRef.current.bottom) {
+        parseInt(imageRef.current.style.top, 10) -
+        Math.ceil(imageRect.top - liveArea.top);
+    } else if (imageRect.bottom < liveArea.bottom) {
       newTop =
-        parseInt(imageRef.current.style.top) +
-        Math.ceil(liveAreaRectRef.current.bottom - imageRect.bottom);
+        parseInt(imageRef.current.style.top, 10) +
+        Math.ceil(liveArea.bottom - imageRect.bottom);
     }
 
     // assign new position
@@ -181,7 +189,11 @@ function ImageUpload() {
       imageRef.current.style.left = newLeft + "px";
     }
 
-    setSliderVal(parseInt(resizeRef.current.value));
+    setSliderVal(parseInt(newVal, 10));
+    setImagePos({
+      top: parseInt(imageRef.current.style.top, 10),
+      left: parseInt(imageRef.current.style.left, 10),
+    });
   };
 
   const mouseDownHandler = (e) => {
@@ -201,38 +213,29 @@ function ImageUpload() {
       <div className="preview-container">
         <div className="preview-container__live-area" ref={liveAreaRef}>
           <img
-            src="/noface_samurai.png"
+            src="/graphics/images/noface_samurai.png"
             alt=""
-            id="draggable-image"
             draggable="false"
             ref={imageRef}
-            onLoad={previewImage}
+            onLoad={setImageDirection}
             onMouseDown={mouseDownHandler}
           />
         </div>
       </div>
       <input
         type="range"
-        id="resize"
         className="slider"
         name="resize-slider"
         min="100"
-        max="400"
+        max={sliderMax}
         step="10"
         value={sliderVal}
-        ref={resizeRef}
         onChange={resizeImage}
       />
-      <button
-        id="reset"
-        className="button"
-        type="button"
-        ref={resetRef}
-        onClick={resetImage}
-      >
+      <button className="button" type="button" onClick={resetImage}>
         Reset
       </button>
-      <button className="button" id="upload" type="button">
+      <button className="button" type="button">
         Upload
       </button>
     </div>
